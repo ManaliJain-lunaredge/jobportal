89edc891b51191541442b56c6bab1b888e97a0a6
ManaliJain-lunaredge <Manali.jain@lunaredge.in>
2026-04-30 19:01:35 +0530

    feat(job-routes): add create and update job routes with validation

--- services/job/src/controller/job.ts
+++ services/job/src/controller/job.ts
@@ -3,7 +3,7 @@ import type { AuthenticatedRequest } from "../middleware/auth.js";
 import getBuffer from "../utils/buffer.js";
 import { sql } from "../utils/db.js";
 import ErrorHandler from "../utils/errorHandler.js";
-import  { TryCatch } from "../utils/TryCatch.js";
+import { TryCatch } from "../utils/TryCatch.js";
 
 export const createCompany = TryCatch(
   async (req: AuthenticatedRequest, res, next) => {
@@ -63,44 +63,44 @@ export const createCompany = TryCatch(
 );
 
 
-export const deleteCompany=TryCatch(async(req:AuthenticatedRequest,res,next)=>{
-    const user=req.user;
-    const {companyId}=req.params;
-    const [company]=await sql`SELECT logo_public_id FROM companies WHERE company_id=${companyId} AND recruiter_id =${user?.user_id} `;
+export const deleteCompany = TryCatch(async (req: AuthenticatedRequest, res, next) => {
+  const user = req.user;
+  const { companyId } = req.params;
+  const [company] = await sql`SELECT logo_public_id FROM companies WHERE company_id=${companyId} AND recruiter_id =${user?.user_id} `;
 
-    if(!company){
-         throw new ErrorHandler(404, "Company not found or you are not authorised to delete");
-    }
+  if (!company) {
+    throw new ErrorHandler(404, "Company not found or you are not authorised to delete");
+  }
 
-await sql`DELETE FROM companies WHERE company_id=${companyId} `
-res.json({
-    message:"Company and all asociated jobs have been deleted"
+  await sql`DELETE FROM companies WHERE company_id=${companyId} `
+  res.json({
+    message: "Company and all asociated jobs have been deleted"
+  })
+
+
 })
 
+export const createJob = TryCatch(async (req: AuthenticatedRequest, res, next) => {
+  const user = req.user;
 
-})
+  if (!user) {
+    throw new ErrorHandler(401, "Authentication failed");
+  }
 
-export const createJob =TryCatch(async(req:AuthenticatedRequest,res,next)=>{
-     const user = req.user;
+  if (user.role !== "recruiter") {
+    throw new ErrorHandler(403, "Forbidden: only recruiter can create company");
+  }
+  const { title, description, salary, location, role, job_type, work_location, company_id, openings } = req.body;
+  if (!title || !description || !salary || !location || !role || !job_type || !work_location || !company_id || !openings) {
+    throw new ErrorHandler(400, "All fields are required")
+  }
+  const [company] = await sql`SELECT company_id FROM companies WHERE company_id=${company_id} AND recruiter_id=${user.user_id}`
+  if (!company) {
+    throw new ErrorHandler(404, "Company not found");
+  }
 
-    if (!user) {
-      throw new ErrorHandler(401, "Authentication failed");
-    }
-
-    if (user.role !== "recruiter") {
-      throw new ErrorHandler(403, "Forbidden: only recruiter can create company");
-    }
-    const {name,description,salary,location,role,job_type,work_location,company_id,openings}=req.body;
- if (!name || !description || !salary||!location||!role||!job_type||!work_location||!company_id||!openings) {
-      throw new ErrorHandler(400, "All fields are required")
-    }
-const [company]=await sql`SELECT company_id FROM companies WHERE company_id=${company_id} AND recruiter_id=${user.user_id}`
-if(!company){
-     throw new ErrorHandler(404, "Company not found");
-}
-
-const [newJob] =
-await sql`
+  const [newJob] =
+    await sql`
   INSERT INTO jobs (
     title, 
     description, 
@@ -114,7 +114,7 @@ await sql`
     openings
   ) 
   VALUES (
-    ${name}, 
+    ${title}, 
     ${description}, 
     ${salary}, 
     ${location}, 
@@ -127,6 +127,56 @@ await sql`
   ) 
   RETURNING *;
 `;
+res.json({
+  message:"Job created Succesfully",
+  job:newJob
+})
 
+
+})
+
+
+
+export const updateJob =TryCatch(async(req:AuthenticatedRequest,res,next)=>{
+    const user = req.user;
+
+  if (!user) {
+    throw new ErrorHandler(401, "Authentication failed");
+  }
+
+  if (user.role !== "recruiter") {
+    throw new ErrorHandler(403, "Forbidden: only recruiter can create company");
+  }
+   const { title, description, salary, location, role, job_type, work_location, company_id, openings ,is_active} = req.body;
+  if (!title || !description || !salary || !location || !role || !job_type || !work_location || !company_id || !openings) {
+    throw new ErrorHandler(400, "All fields are required")
+  }
+
+
+
+  const [exsitngJob]=await sql`SELECT posted_by_recruiter_id FROM jobs WHERE job_id=${req.params.jobId} `
+
+
+  if(!exsitngJob){
+    throw new ErrorHandler(404, "Job Not Found")
+  }
+
+  if(exsitngJob.posted_by_recruiter_id !==user.user_id){
+    throw new ErrorHandler(403, "Forbidden : not allowed to update")
+  }
+const [updatedJob]=await sql`UPDATE jobs SET title=${title} ,description = ${description},
+salary = ${salary},
+location = ${location},
+role = ${role},
+job_type = ${job_type},
+work_location = ${work_location},
+openings = ${openings},
+is_active = ${is_active}
+WHERE job_id = ${req.params.jobId} RETURNING *;`
+
+res.json({
+  message:"job updated",
+  job:updatedJob
+})
 
 })
--- services/job/src/routes/index.ts
+++ services/job/src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from "express"
-import { createCompany, deleteCompany } from "../controller/job.js"
+import { createCompany, createJob, deleteCompany, updateJob } from "../controller/job.js"
 import { isAuth } from "../middleware/auth.js"
 import uploadFile from "../middleware/multer.js"
 const router = express.Router()
 
 router.post("/create-company", isAuth, uploadFile, createCompany)
 router.delete("/delete-company/:companyId",isAuth,deleteCompany)
+router.post("/create-job",isAuth,createJob)
+router.post("/update-job/:jobId",isAuth,updateJob)
 export default router
